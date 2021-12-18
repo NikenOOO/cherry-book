f3d44d7c7bb5945528e2dbb4ba95fc80fb48e95e
NikenOOO <kouhei23.0927@gmail.com>
2021-12-18 18:05:20 +0900

    section8 about module

---
+++ lib/bank.rb
@@ -0,0 +1,11 @@
+require './lib/deep_freezable'
+
+class Bank
+  extend DeepFreezable
+
+  CURRENCIES = deep_freezable({
+    'Japan' => 'yen',
+    'US'    => 'dollar',
+    'India' => 'rupee'
+    })
+end
